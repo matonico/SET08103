92e57f590b6b3b0b779f827ed55d85bf0a992d9d
Matthew Lenathen <thisiswbb@gmail.com>
2022-02-16 23:37:32 +0000

    Added sql statement to answer issue #6, updated tags

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -14,6 +14,10 @@ public class App
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
@@ -93,9 +97,12 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
+                   /* "SELECT emp_no, first_name, last_name "
                             + "FROM employees "
                             + "WHERE emp_no = " + ID;
+
+                    */
+                "SELECT e.emp_no,e.first_name, e.last_name,s.salary,t.title,d.dept_name,ee.first_name AS mf,ee.last_name AS ml FROM employees AS e join salaries AS s ON (e.emp_no=s.emp_no) join titles AS t ON (e.emp_no=t.emp_no) join dept_emp AS de ON (e.emp_no=de.emp_no) join departments as d ON (de.dept_no=d.dept_no) join dept_manager as dm ON (d.dept_no=dm.dept_no) join employees as ee ON (dm.emp_no=ee.emp_no) WHERE e.emp_no=255530 AND s.to_date='9999-01-01' AND dm.to_date='9999-01-01'";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -105,6 +112,10 @@ public class App
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.salary = rset.getInt("salary");
+                emp.title=rset.getString("title");
+                emp.dept_name=rset.getString("dept_name");
+                emp.manager=rset.getString("mf")+" "+rset.getString("ml");
                 return emp;
             } else
                 return null;
@@ -112,6 +123,25 @@ public class App
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
             return null;
+        }
+    }
+
+    /**
+     * Display employee class
+     * @param emp
+     */
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + "Department: "+ emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 }
