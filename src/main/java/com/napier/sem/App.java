c672e420356d855148ac460aa7e09b584dd592d7
Matthew Lenathen <thisiswbb@gmail.com>
2022-02-17 14:00:11 +0000

    added feature to print list of all salaries, updated version number in files

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -19,6 +19,9 @@ public class App
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
 
+        // Use print salaries function
+        a.printSalaries(employees);
+
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
 
@@ -185,6 +188,23 @@ public class App
             System.out.println(e.getMessage());
             System.out.println("Failed to get salary details");
             return null;
+        }
+    }
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
         }
     }
 }
