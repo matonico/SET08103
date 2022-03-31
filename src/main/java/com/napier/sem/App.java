6d35fdebcb09b971d2025f30e01d25045c5f6bed
matthew <thisiswbb@gmail.com>
2022-03-31 20:14:09 +0100

    added new int test

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -18,13 +18,13 @@ public class App
             a.connect(args[0], Integer.parseInt(args[1]));
         }
 
-        Department dept = a.getDepartment("Development");
-        ArrayList<Employee> employees = a.getSalariesByDepartment(dept);
-
+        //Department dept = a.getDepartment("Development");
+       // ArrayList<Employee> employees = a.getSalariesByDepartment(dept);
+        Employee e = a.getEmployee(500000);
 
         // Print salary report
-        a.printSalaries(employees);
-
+      //  a.printSalaries(employees);
+    a.displayEmployee(e);
         // Disconnect from database
         a.disconnect();
     }
@@ -89,35 +89,34 @@ public class App
      * @param ID
      * @return emp
      */
-    public Employee getEmployee(int ID) {
-        try {
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                   /* "SELECT emp_no, first_name, last_name "
+                    "SELECT emp_no, first_name, last_name "
                             + "FROM employees "
                             + "WHERE emp_no = " + ID;
-
-                    */
-                "SELECT e.emp_no,e.first_name, e.last_name,s.salary,t.title,d.dept_name,ee.first_name AS mf,ee.last_name AS ml FROM employees AS e join salaries AS s ON (e.emp_no=s.emp_no) join titles AS t ON (e.emp_no=t.emp_no) join dept_emp AS de ON (e.emp_no=de.emp_no) join departments as d ON (de.dept_no=d.dept_no) join dept_manager as dm ON (d.dept_no=dm.dept_no) join employees as ee ON (dm.emp_no=ee.emp_no) WHERE e.emp_no=255530 AND s.to_date='9999-01-01' AND dm.to_date='9999-01-01'";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
             // Check one is returned
-            if (rset.next()) {
+            if (rset.next())
+            {
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
-                emp.salary = rset.getInt("salary");
-                emp.title=rset.getString("title");
-                emp.dept_name=rset.getString("dept_name");
-                emp.manager=rset.getString("mf")+" "+rset.getString("ml");
                 return emp;
-            } else
+            }
+            else
                 return null;
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
             return null;
@@ -290,6 +289,14 @@ public class App
             return null;
         }
     }
+
+    /**
+     * t
+     * @param e
+     * @param rset
+     * @return
+     * @throws SQLException
+     */
     public ArrayList<Employee> addEmployeesToList(ArrayList<Employee> e, ResultSet rset) throws SQLException {
         while (rset.next())
         {
@@ -301,5 +308,23 @@ public class App
             e.add(emp);
         }
         return e;
+    }
+
+    public void addEmployee(Employee emp)
+    {
+        try
+        {
+            Statement stmt = con.createStatement();
+            String strUpdate =
+                    "INSERT INTO employees (emp_no, first_name, last_name, birth_date, gender, hire_date) " +
+                            "VALUES (" + emp.emp_no + ", '" + emp.first_name + "', '" + emp.last_name + "', " +
+                            "'9999-01-01', 'M', '9999-01-01')";
+            stmt.execute(strUpdate);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to add employee");
+        }
     }
 }
--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -23,10 +23,10 @@ public class AppIntegrationTest
     @Test
     void testGetEmployee()
     {
-        Employee emp = app.getEmployee(255530);
-        assertEquals(emp.emp_no, 255530);
-        assertEquals(emp.first_name, "Ronghao");
-        assertEquals(emp.last_name, "Garigliano");
+        Employee emp = app.getEmployee(255655);
+        assertEquals(emp.emp_no, 255655);
+        assertEquals(emp.first_name, "Khaled");
+        assertEquals(emp.last_name, "Eastman");
     }
     @Test
     void testGetDepartment()
@@ -35,4 +35,19 @@ public class AppIntegrationTest
         assertEquals(dep.dept_name,"Sales");
         assertEquals(dep.dept_no,"d007");
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
+
 }
